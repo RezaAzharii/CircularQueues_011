5b6111b6f54ae2030e3728c3cbd8d5f638d15eb2
RezaAzharii <rezaazhari867@gmail.com>
2023-06-22 10:49:18 +0700

    melakukan pengecekan apakah antrian kosong atau tidak

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -18,10 +18,17 @@ public:
 		cout << endl;
 
 		//Cek apakah antrian penuh
-		if ((FRONT == 0 && max - 1) || (FRONT == REAR + 1)) {
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
 			cout << "\nQueue overflow\n";
 			return;
 		}
+		
+		//Cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		
 	}
 	
 };
